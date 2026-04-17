4933ca112627c30eb53eca511603828f50183a61
Harini RB <harinirajahrb@gmail.com>
2026-04-17 21:59:43 +0530

    Time: 5 ms (48.51%) | Memory: 46.6 MB (95.78%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer , Integer > freq = new HashMap<>();
+        int res [] = new int[2];
+
+        for(int i = 0 ; i < nums.length ; i++){
+            if(freq.containsKey(target - nums[i])){
+                res[0] = freq.get(target - nums[i]);
+                res[1] = i ;
+            }
+            else{
+                freq.put(nums[i] , i);
+            }
+        }
+        
+        return res;
+    }
+}
